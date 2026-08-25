8957eb4403d6af60da34cdc1067312de0f95737d
nixOSp16s <bischisseggsdungeon@gmail.com>
2026-08-25 23:47:54 +0200

    sync 2026-08-25

---
+++ nixos/spicetify.nix
@@ -0,0 +1,16 @@
+{ pkgs, spicetify-nix, ... }:
+let
+  spicePkgs = spicetify-nix.legacyPackages.${pkgs.system};
+in
+{
+  programs.spicetify = {
+    enable = true;
+    theme = spicePkgs.themes.catppuccin;
+    colorScheme = "mocha";
+    enabledExtensions = with spicePkgs.extensions; [
+      adblock
+      hidePodcasts
+      shuffle
+    ];
+  };
+}
